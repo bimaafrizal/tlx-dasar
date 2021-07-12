a4903b50966f223d89b27b119d51340299abef1d
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2021-07-13 00:00:46 +0700

    Add files via upload

---
+++ transposeMatrix_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class transposeMatrix_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, e, f, g, h, i;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        e = sc.nextInt();
+        f = sc.nextInt();
+        g = sc.nextInt();
+        h = sc.nextInt();
+        i = sc.nextInt();
+
+        System.out.println(a + " " + d + " " + g);
+        System.out.println(b + " " + e + " " + h);
+        System.out.println(c + " " + f + " " + i);
+    }
+}
