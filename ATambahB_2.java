ddcddfcaa55367b084c8a89a18daeaaebab3b5b5
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2021-07-12 23:59:25 +0700

    Add files via upload

---
+++ ATambahB_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ATambahB_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jantan, betina;
+
+        // System.out.println("Masukan jumlah bebek!");
+        jantan = input.nextInt();
+        betina = input.nextInt();
+
+        System.out.println(jantan + betina);
+    }
+}
