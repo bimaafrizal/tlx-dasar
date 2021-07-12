bd1df99e28dec0d350006c6159bb9903ce406c62
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2021-07-12 23:59:46 +0700

    Add files via upload

---
+++ burungBeo_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class burungBeo_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputan;
+
+        inputan = sc.nextLine();
+        System.out.println(inputan);
+    }
+}
